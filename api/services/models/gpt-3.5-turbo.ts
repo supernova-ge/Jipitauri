9d6ab1ccb1cfa90867f3b267d1cbd0098407dae2
ningfu1 <nino.mirziashvili7@gmail.com>
2023-03-16 17:20:29 +0400

    feat: turbo model summarization

--- api/services/models/gpt-3.5-turbo.ts
+++ api/services/models/gpt-3.5-turbo.ts
@@ -16,6 +16,35 @@ export default class Turbo {
       const { data } = await this.openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages,
+        temperature: 0.2,
+        max_tokens: 1000,
+      });
+
+      console.log({
+        model: "gpt-3.5-turbo",
+        messages,
+      });
+
+      return data.choices[0].message?.content as string;
+    } catch (e) {
+      return "";
+    }
+  }
+
+  async getSummary(uin: string) {
+    let messages: ChatCompletionRequestMessage[] = [
+      {
+        role: "user",
+        content: uin + `\nTl;dr\n`,
+      },
+    ];
+
+    try {
+      const { data } = await this.openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages,
+        temperature: 0.2,
+        max_tokens: 1000,
       });
 
       console.log({
